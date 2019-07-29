9d3d07852814f0dffb789e9cfaaee0795ee37b81
Taylor <stein0209@gmail.com>
2019-07-29 16:44:05 -0600

    addes taco hash

---
+++ TacosRapidos/tacos_hash.rb
@@ -0,0 +1,221 @@
+tacos = [
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+    name: "string",
+    protein: "string",
+    heat_level: 1-3,
+    shell_type: "string",
+    drink_paring: "beer"
+    },
+    {
+    name: "string",
+    protein: "string",
+    heat_level: 1-3,
+    shell_type: "string",
+    drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+        },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+    {
+        name: "string",
+        protein: "string",
+        heat_level: 1-3,
+        shell_type: "string",
+        drink_paring: "beer"
+    },
+        
+       
+]
