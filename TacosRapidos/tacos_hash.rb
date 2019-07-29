27db32a4218453c4c1515944269efbd7286fce0f
Taylor <stein0209@gmail.com>
2019-07-29 17:30:00 -0600

    adds beef and chicken tacos

--- TacosRapidos/tacos_hash.rb
+++ TacosRapidos/tacos_hash.rb
@@ -1,5 +1,89 @@
 tacos = [
     {
+        name: "Asian Marinated Chicken",
+        protein: "chicken",
+        heat_level: 1,
+        shell_type: "soft",
+        drink_paring: "beer"
+    },
+    {
+        name: "Cilantro Lime Chicken",
+        protein: "chicken",
+        heat_level: 1,
+        shell_type: "crunchy",
+        drink_paring: "sangria"
+    },
+    {
+        name: "Honey Chipotle Chicken",
+        protein: "chicken",
+        heat_level: 2,
+        shell_type: "soft",
+        drink_paring: "beer"
+    },
+    {
+        name: "Grilled Chicken and Avocado",
+        protein: "chicken",
+        heat_level: 1,
+        shell_type: "crispy",
+        drink_paring: "margarita"
+    },
+    {
+        name: "Chili Lime Chicken",
+        protein: "chicken",
+        heat_level: 3,
+        shell_type: "crispy",
+        drink_paring: "margarita"
+    },
+    {
+        name: "Korean Fried Chicken",
+        protein: "chicken",
+        heat_level: 1,
+        shell_type: "crispy",
+        drink_paring: "sangria"
+    },
+    {
+        name: "Ground Beef",
+        protein: "beef",
+        heat_level: 1,
+        shell_type: "soft",
+        drink_paring: "beer"
+    },
+    {
+        name: "Spicy Shredded Beef",
+        protein: "beef",
+        heat_level: 3,
+        shell_type: "soft",
+        drink_paring: "beer"
+    },
+    {
+        name: "Carne Asada",
+        protein: "beef",
+        heat_level: 1,
+        shell_type: "crispy",
+        drink_paring: "margarita"
+    },
+    {
+        name: "Beef Fajita",
+        protein: "beef",
+        heat_level: 1,
+        shell_type: "crispy",
+        drink_paring: "margarita"
+    },
+    {
+        name: "Ropa Vieja",
+        protein: "beef",
+        heat_level: 2,
+        shell_type: "soft",
+        drink_paring: "sangria"
+    },
+    {
+        name: "Barbacoa Beef",
+        protein: "beef",
+        heat_level: 3,
+        shell_type: "soft",
+        drink_paring: "sangria"
+    },
+    {
         name: "string",
         protein: "string",
         heat_level: 1-3,
@@ -89,90 +173,6 @@ tacos = [
         heat_level: 1-3,
         shell_type: "string",
         drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-        name: "string",
-        protein: "string",
-        heat_level: 1-3,
-        shell_type: "string",
-        drink_paring: "beer"
-    },
-    {
-    name: "string",
-    protein: "string",
-    heat_level: 1-3,
-    shell_type: "string",
-    drink_paring: "beer"
-    },
-    {
-    name: "string",
-    protein: "string",
-    heat_level: 1-3,
-    shell_type: "string",
-    drink_paring: "beer"
     },
     {
         name: "string",
