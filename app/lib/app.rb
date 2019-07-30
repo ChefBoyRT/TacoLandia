2d503259c71df054c46149844c41777eae965440
catherine-o <catherine.ohara@outlook.com>
2019-07-30 17:45:52 -0600

    added main menu, displays taco list, random taco

--- app/lib/app.rb
+++ app/lib/app.rb
@@ -4,16 +4,30 @@ def welcome_user
 end
 
 def runner
+    #Welcomes user or creates new user
     prompt = TTY::Prompt.new
     welcome_user
     names = User.get_user_names
     names << "Create new user"
-    selection = prompt.select("What is your name friend?", names)
+    name_selection = prompt.select("What is your name friend?", names)
     # binding.pry
-    if selection != "Create new user"
-        puts "Welcome back #{selection}!"
+    if name_selection != "Create new user"
+        puts "Welcome back #{name_selection}!"
     else
         new_user = User.create_user_name 
     end
+
+    #display main menu
+    menu_options = ["Show me a list of tacos", "Choose a random taco for me"]
+    menu_selection = prompt.select("Please choose an option:", menu_options)
+    if menu_selection == "Show me a list of tacos"
+        taco_names = Taco.get_taco_names
+        taco_names.each do |taco|
+            puts taco 
+        end
+    elsif menu_selection == "Choose a random taco for me"
+        print Taco.get_taco_names.sample + " Taco"
+    end
+    
     
 end
--- app/lib/taco.rb
+++ app/lib/taco.rb
@@ -2,4 +2,12 @@ class Taco < ActiveRecord::Base
     belongs_to :drink
     has_many :meals
     has_many :users, through: :meals
+
+    def self.get_taco_names
+        all.pluck(:name).sort
+        # taco_names.map do |taco|
+        #     p taco
+        # end
+    end
+
 end
