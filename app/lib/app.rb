7062eee3cc05cb4efc7586a1f37d54999e43a145
catherine-o <catherine.ohara@outlook.com>
2019-07-31 12:19:44 -0600

    added sleeps and clears, fixed create user

--- app/lib/app.rb
+++ app/lib/app.rb
@@ -1,38 +1,50 @@
 def welcome_user
     puts "Welcome to TacoLandia!!!"
-    puts "Tell us who you are"
+    sleep(1.5)
+    system "clear"
+    puts "Tell us who you are!!!"
 end
 
 def runner
-    exit_program = false
+    system "clear"
     #Welcomes user or creates new user
     prompt = TTY::Prompt.new
     welcome_user
+    sleep(1.5)
+    system "clear"
     user_names = User.get_user_names
     user_names << "Create new user"
     user_names << "Exit program"
     user_name_selection = prompt.select("What is your name friend?", user_names)
+    
     # binding.pry
-    if user_names.include?(user_name_selection) && user_name_selection != "Exit program"
+    if user_names.include?(user_name_selection) && user_name_selection != "Exit program" && user_name_selection != "Create new user"
         puts "Welcome back #{user_name_selection}!"
     elsif user_name_selection == "Create new user"
         new_user = User.create_user_name 
+        sleep(1.5)
+        system "clear"
     elsif user_name_selection == "Exit program"
         abort "Goodbye!"
     end
+    
 
     #display main menu
     
     menu_options = ["Show me a list of tacos", "Choose a random taco for me", "Pair taco to drink", "See your favorite tacos", "Exit Program"]
     menu_selection = prompt.select("Please choose an option:", menu_options)
-    
+    system "clear"
     loop do
         case menu_selection
         when "Show me a list of tacos"
             taco_names = Taco.get_taco_names
             taco_names.each do |taco|
                 puts taco 
+                sleep(0.25)
             end
+            sleep(1)
+            system "clear"
+            menu_selection = prompt.select("Please choose an option:", menu_options)
         
         when "Choose a random taco for me"
             random_taco_return = Taco.get_taco_names.sample
@@ -40,6 +52,9 @@ def runner
             response = prompt.select("Please choose an option:", ["See taco details", "Save taco", "Return to main menu"])
             if response == "See taco details"
                 Taco.get_taco_details(random_taco_return)
+                sleep(5)
+                system "clear"
+                menu_selection = prompt.select("Please choose an option:", menu_options)
             elsif response == "Save taco"
                 taco_id = Taco.get_taco_id(random_taco_return)
                 user_id = User.get_user_id(user_name_selection)
@@ -47,11 +62,13 @@ def runner
                 puts "AWESOME"
                 sleep(1)
                 puts "Your #{random_taco_return} Taco has been saved!!!"
-                sleep(1)
+                sleep(1.25)
                 system "clear"
                 menu_selection = prompt.select("Please choose an option:", menu_options)
+                system "clear"
             elsif response == "Return to main menu"
                 menu_selection = prompt.select("Please choose an option:", menu_options)
+                system "clear"
             end
         
         when "Pair taco to drink"
@@ -59,19 +76,29 @@ def runner
             drink_names << "Main Menu"
             drink_selection = prompt.select("Please choose an option:", drink_names)
             
-            if drink_selection == "Beer"
+            case drink_selection
+            when "Beer"
                 Drink.get_tacos_for_beer
-            elsif drink_selection == "Classic Margarita"
+                sleep(4)
+                system "clear"
+                
+            when "Classic Margarita"
                 Drink.get_tacos_for_margarita
-            elsif drink_selection == "Sangria"
+                
+
+            when "Sangria"
                 Drink.get_tacos_for_sangria
+                sleep(5)
+                system "clear"
+
             else 
                 menu_selection = prompt.select("Please choose an option:", menu_options)
             end
+
         when "Exit Program"
             abort "Goodbye!"
         else
-            exit_program = true
+            abort "Goodbye!"
         end
     end
     
