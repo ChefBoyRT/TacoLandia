eb2db1af1529cb0bfb5469c2bccf0dd0eee3de9f
catherine-o <catherine.ohara@outlook.com>
2019-07-30 18:23:21 -0600

    can pair taco by drink

--- app/lib/app.rb
+++ app/lib/app.rb
@@ -7,27 +7,42 @@ def runner
     #Welcomes user or creates new user
     prompt = TTY::Prompt.new
     welcome_user
-    names = User.get_user_names
-    names << "Create new user"
-    name_selection = prompt.select("What is your name friend?", names)
+    user_names = User.get_user_names
+    user_names << "Create new user"
+    user_name_selection = prompt.select("What is your name friend?", user_names)
     # binding.pry
-    if name_selection != "Create new user"
-        puts "Welcome back #{name_selection}!"
+    if user_name_selection != "Create new user"
+        puts "Welcome back #{user_name_selection}!"
     else
         new_user = User.create_user_name 
     end
 
     #display main menu
-    menu_options = ["Show me a list of tacos", "Choose a random taco for me"]
+    menu_options = ["Show me a list of tacos", "Choose a random taco for me", "Pair taco to drink"]
     menu_selection = prompt.select("Please choose an option:", menu_options)
     if menu_selection == "Show me a list of tacos"
         taco_names = Taco.get_taco_names
         taco_names.each do |taco|
             puts taco 
         end
+    
     elsif menu_selection == "Choose a random taco for me"
         print Taco.get_taco_names.sample + " Taco"
-    end
     
+    elsif menu_selection == "Pair taco to drink"
+        drink_names = Drink.get_drink_names
+        drink_selection = prompt.select("Please choose an option:", drink_names)
+        
+        if drink_selection == "Beer"
+            Drink.get_tacos_for_beer
+        elsif drink_selection == "Classic Margarita"
+            Drink.get_tacos_for_margarita
+        elsif drink_selection == "Sangria"
+            Drink.get_tacos_for_sangria
+        end
+
+    end
+
+
     
 end
--- app/lib/drink.rb
+++ app/lib/drink.rb
@@ -1,3 +1,33 @@
 class Drink < ActiveRecord::Base
     has_many :tacos
+
+    
+    def self.get_drink_names
+        all.pluck(:name).sort
+    end
+
+    def self.get_tacos_for_beer
+        beer = Drink.find_by(name: "Beer")
+        paired = beer.tacos.pluck(:name).sort
+        paired.each do |taco|
+            print taco + "\n"
+        end
+    end
+
+    def self.get_tacos_for_margarita
+        margarita = Drink.find_by(name: "Classic Margarita")
+        paired = margarita.tacos.pluck(:name).sort
+        paired.each do |taco|
+            print taco + "\n"
+        end
+    end
+
+    def self.get_tacos_for_sangria
+        sangria = Drink.find_by(name: "Sangria")
+        paired = sangria.tacos.pluck(:name).sort
+        paired.each do |taco|
+            print taco + "\n"
+        end
+    end
+
 end
