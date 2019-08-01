6617223a17758c2094695c1074b0712545e716f6
Taylor <stein0209@gmail.com>
2019-07-31 18:04:15 -0600

    adds gems for stylizing

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,11 @@ require 'sinatra/activerecord'
 require 'require_all'
 require 'pry'
 require 'tty-prompt'
+require 'colorize'
+require 'colorized_string'
+require 'tty-box'
+require 'tty-cursor'
+require 'tty-screen'
 
 require_all 'app'
 
@@ -12,7 +17,14 @@ ActiveRecord::Base.establish_connection(
 
 ActiveRecord::Base.logger = nil
 
-# runner
 
-binding.pry
+# binding.pry
 
+# box
+# https://github.com/piotrmurach/tty-box#1-usage
+
+# cursor
+# https://github.com/piotrmurach/tty-cursor
+
+# blog
+# https://medium.com/@sachafrosell/tty-a-ruby-terminal-apps-toolkit-ba66f67ca667
